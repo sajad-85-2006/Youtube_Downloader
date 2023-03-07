aded93b29b67f1a84ea3fe6c3335b33ec6a1e90f
sajad-85-2006 <104766821+sajad-85-2006@users.noreply.github.com>
2023-03-07 22:19:50 +0330

    Finish Router
    Signed-off-by: sajad-85-2006 <104766821+sajad-85-2006@users.noreply.github.com>

---
+++ app/Http/Controllers/VideoController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\VideoResource;
+use App\Models\Video;
+use Illuminate\Http\Request;
+
+class VideoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(new VideoResource(Video::all()));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function getVideo(string $id)
+    {
+        return response()->json();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller('\App\Http\Controllers\VideoController')->group(function () {
+    Route::get('/video', 'index');
+    Route::post('/video/download', 'download');
 });
